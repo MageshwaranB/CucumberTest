bf08b049a8dca3a013730051046d848c04078d67
MageshwaranB <88375878+MageshwaranB@users.noreply.github.com>
2021-08-04 07:03:00 +0530

    Update LoginStepDef.java

--- SeleniumCucumber/src/test/java/stepdefinition/LoginStepDef.java
+++ SeleniumCucumber/src/test/java/stepdefinition/LoginStepDef.java
@@ -16,24 +16,19 @@ import org.junit.Assert;
 public class LoginStepDef {
 	WebDriver driver;
 
-	@Given("User is on login page")
+	@Before
+	public void setup(){
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
 
 	@Given("User is on Login Page")
 	public void user_is_on_login_page() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		
 		driver.get("https://www.saucedemo.com/");
 	}
 
-//	@When("User enters username")
-//	public void user_enters_username() {
-//		driver.findElement(By.id("user-name")).sendKeys("standard_user");
-//	}
-//
-//	@When("User enters password")
-//	public void user_enters_password() {
-//		driver.findElement(By.id("password")).sendKeys("secret_sauce");
-//	}
+
 	@When("^: User enters (.+) and (.+)$")
 	public void _user_enters_and(String username, String password) {
 		driver.findElement(By.id("user-name")).sendKeys(username);
@@ -58,6 +53,10 @@ public class LoginStepDef {
 		String actText=driver.findElement(By.xpath("//div[text()='Sauce Labs Backpack']")).getText();
 		String expText="Sauce Labs Backpack";
 		Assert.assertEquals(actText,expText);
+		
+	}
+	@After
+	public void setupclose(){
 		driver.close();
 	}
 	
